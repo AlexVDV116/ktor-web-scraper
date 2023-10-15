75376217f54e5d03ecca3074ed57966707ec3217
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-10-15 22:36:56 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+val ktorVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("org.jsoup:jsoup:1.16.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
